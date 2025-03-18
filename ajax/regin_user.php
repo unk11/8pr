0685701106d4e3a86d4f295e9af9f261c0f81be2
unk <ununk@DESKTOP-NH0291J>
2025-03-18 23:58:02 +0500

    Шаг 4. При регистрации пользователя реализовал проверку пароля по следующим условиям (со стороны JavaScript и PHP). Прохоров

--- ajax/regin_user.php
+++ ajax/regin_user.php
@@ -1,24 +1,33 @@
 <?php
-	session_start();
-	include("../settings/connect_datebase.php");
-	
-	$login = $_POST['login'];
-	$password = $_POST['password'];
-	
-	// ищем пользователя
-	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."'");
-	$id = -1;
-	
-	if($user_read = $query_user->fetch_row()) {
-		echo $id;
-	} else {
-		$mysqli->query("INSERT INTO `users`(`login`, `password`, `roll`) VALUES ('".$login."', '".$password."', 0)");
-		
-		$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
-		$user_new = $query_user->fetch_row();
-		$id = $user_new[0];
-			
-		if($id != -1) $_SESSION['user'] = $id; // запоминаем пользователя
-		echo $id;
-	}
+session_start();
+include("../settings/connect_datebase.php");
+
+$login = $_POST['login'];
+$password = $_POST['password'];
+
+// Ищем пользователя с таким логином
+$query_user = $mysqli->query("SELECT * FROM users WHERE login='".$login."'");
+$id = -1;
+
+// Проверка на существование логина
+if($user_read = $query_user->fetch_row()) {
+    echo $id;  // Логин занят
+} else {
+    // Хешируем пароль
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    
+    // Вставляем нового пользователя
+    if ($mysqli->query("INSERT INTO users(login, password, roll) VALUES ('".$login."', '".$hashed_password."', 0)")) {
+        $query_user = $mysqli->query("SELECT * FROM users WHERE login='".$login."'");
+        $user_new = $query_user->fetch_row();
+        $id = $user_new[0];
+
+        if($id != -1) {
+            $_SESSION['user'] = $id; // Запоминаем пользователя
+        }
+        echo $id;
+    } else {
+        echo 0;  // Ошибка регистрации
+    }
+}
 ?>
--- regin.php
+++ regin.php
@@ -4,7 +4,6 @@
 	
 	if (isset($_SESSION['user'])) {
 		if($_SESSION['user'] != -1) {
-			
 			$user_query = $mysqli->query("SELECT * FROM `users` WHERE `id` = ".$_SESSION['user']);
 			while($user_read = $user_query->fetch_row()) {
 				if($user_read[3] == 0) header("Location: user.php");
@@ -13,121 +12,127 @@
 		}
  	}
 ?>
-<html>
-	<head> 
-		<meta charset="utf-8">
-		<title> Регистрация </title>
-		
-		<script src="https://code.jquery.com/jquery-1.8.3.js"></script>
-		<link rel="stylesheet" href="style.css">
-	</head>
-	<body>
-		<div class="top-menu">
-			<a href=#><img src = "img/logo1.png"/></a>
-			<div class="name">
-				<a href="index.php">
-					<div class="subname">БЗОПАСНОСТЬ  ВЕБ-ПРИЛОЖЕНИЙ</div>
-					Пермский авиационный техникум им. А. Д. Швецова
-				</a>
-			</div>
-		</div>
-		<div class="space"> </div>
-		<div class="main">
-			<div class="content">
-				<div class = "login">
-					<div class="name">Регистрация</div>
-				
-					<div class = "sub-name">Логин:</div>
-					<input name="_login" type="text" placeholder="" onkeypress="return PressToEnter(event)"/>
-					<div class = "sub-name">Пароль:</div>
-					<input name="_password" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
-					<div class = "sub-name">Повторите пароль:</div>
-					<input name="_passwordCopy" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
-					
-					<a href="login.php">Вернуться</a>
-					<input type="button" class="button" value="Зайти" onclick="RegIn()" style="margin-top: 0px;"/>
-					<img src = "img/loading.gif" class="loading" style="margin-top: 0px;"/>
-				</div>
-				
-				<div class="footer">
-					© КГАПОУ "Авиатехникум", 2020
-					<a href=#>Конфиденциальность</a>
-					<a href=#>Условия</a>
-				</div>
-			</div>
-		</div>
-		
-		<script>
-			var loading = document.getElementsByClassName("loading")[0];
-			var button = document.getElementsByClassName("button")[0];
-			
-			function RegIn() {
-				var _login = document.getElementsByName("_login")[0].value;
-				var _password = document.getElementsByName("_password")[0].value;
-				var _passwordCopy = document.getElementsByName("_passwordCopy")[0].value;
-				
-				if(_login != "") {
-					if(_password != "") {
-						if(_password == _passwordCopy) {
-							loading.style.display = "block";
-							button.className = "button_diactive";
-							
-							var data = new FormData();
-							data.append("login", _login);
-							data.append("password", _password);
-							
-							// AJAX запрос
-							$.ajax({
-								url         : 'ajax/regin_user.php',
-								type        : 'POST', // важно!
-								data        : data,
-								cache       : false,
-								dataType    : 'html',
-								// отключаем обработку передаваемых данных, пусть передаются как есть
-								processData : false,
-								// отключаем установку заголовка типа запроса. Так jQuery скажет серверу что это строковой запрос
-								contentType : false, 
-								// функция успешного ответа сервера
-								success: function (_data) {
-									console.log("Авторизация прошла успешно, id: " +_data);
-									if(_data == -1) {
-										alert("Пользователь с таким логином существует.");
-										loading.style.display = "none";
-										button.className = "button";
-									} else {
-										location.reload();
-										loading.style.display = "none";
-										button.className = "button";
-									}
-								},
-								// функция ошибки
-								error: function( ){
-									console.log('Системная ошибка!');
-									loading.style.display = "none";
-									button.className = "button";
-								}
-							});
-						} else alert("Пароли не совподают.");
-					} else alert("Введите пароль.");
-				} else alert("Введите логин.");
-			}
-			
-			function PressToEnter(e) {
-				if (e.keyCode == 13) {
-					var _login = document.getElementsByName("_login")[0].value;
-					var _password = document.getElementsByName("_password")[0].value;
-					var _passwordCopy = document.getElementsByName("_passwordCopy")[0].value;
-					
-					if(_password != "") {
-						if(_login != "") {
-							if(_passwordCopy != "") {
-								RegIn();
-							}
-						}
-					}
-				}
-			}
-			
-		</script>
-	</body>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Регистрация</title>
+    <script src="https://code.jquery.com/jquery-1.8.3.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="top-menu">
+        <a href=#><img src="img/logo1.png" /></a>
+        <div class="name">
+            <a href="index.php">
+                <div class="subname">БЕЗОПАСНОСТЬ ВЕБ-ПРИЛОЖЕНИЙ</div>
+                Пермский авиационный техникум им. А. Д. Швецова
+            </a>
+        </div>
+    </div>
+    <div class="space"> </div>
+    <div class="main">
+        <div class="content">
+        <div class="login">
+            <div class="name">Регистрация</div>
+
+            <div class="sub-name">Почта:</div>
+            <input name="_login" type="text" placeholder="" onkeypress="return PressToEnter(event)" />
+
+            <div class="sub-name">Пароль:</div>
+            <input name="_password" type="password" placeholder="" onkeypress="return PressToEnter(event)" />
+
+            <div class="sub-name">Повторите пароль:</div>
+            <input name="_passwordCopy" type="password" placeholder="" onkeypress="return PressToEnter(event)" />
+
+            <div id="password-errors" style="color: red;"></div>
+            <a href="login.php">Вернуться</a>
+            <input type="button" class="button" value="Зарегистрироваться" onclick="RegIn()" style="margin-top: 0px;" />
+            <img src="img/loading.gif" class="loading" style="margin-top: 0px; display: none;" />
+        </div>
+
+            <div class="footer">
+                © КГАПОУ "Авиатехникум", 2020
+                <a href="#">Конфиденциальность</a>
+                <a href="#">Условия</a>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function showAlert(message) {
+            alert(message);
+        }
+
+        function validatePassword(password) {
+            let errors = [];
+            if (password.length < 8) errors.push("Пароль должен содержать более 8 символов.");
+            if (!/[A-Z]/.test(password)) errors.push("Пароль должен содержать хотя бы одну заглавную букву.");
+            if (!/[a-z]/.test(password)) errors.push("Пароль должен содержать хотя бы одну строчную букву.");
+            if (!/[0-9]/.test(password)) errors.push("Пароль должен содержать хотя бы одну цифру.");
+            if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) errors.push("Пароль должен содержать хотя бы один специальный символ.");
+            return errors;
+        }
+
+        function RegIn() {
+    var _login = document.getElementsByName("_login")[0].value;
+    var _password = document.getElementsByName("_password")[0].value;
+    var _passwordCopy = document.getElementsByName("_passwordCopy")[0].value;
+    var errorDiv = document.getElementById("password-errors");
+    errorDiv.innerHTML = "";
+
+    if (_login === "") {
+        showAlert("Введите логин.");
+        return;
+    }
+
+    if (_password === "") {
+        showAlert("Введите пароль.");
+        return;
+    }
+
+    if (_password !== _passwordCopy) {
+        showAlert("Пароли не совпадают.");
+        return;
+    }
+
+    let passwordErrors = validatePassword(_password);
+    if (passwordErrors.length > 0) {
+        showAlert(passwordErrors.join("\n"));
+        return;
+    }
+
+    document.querySelector(".loading").style.display = "block";
+    var data = new FormData();
+    data.append("login", _login);
+    data.append("password", _password);
+    
+    $.ajax({
+        url: 'ajax/regin_user.php',
+        type: 'POST',
+        data: data,
+        cache: false,
+        processData: false,
+        contentType: false,
+        success: function (_data) {
+            if (_data == -1) {
+                showAlert("Пользователь с таким логином существует.");
+            } else if (_data == 0) {
+                showAlert("Ошибка регистрации. Попробуйте снова.");
+            } else {
+                showAlert("Регистрация успешна!");
+                location.reload();
+            }
+            document.querySelector(".loading").style.display = "none";
+        },
+        error: function() {
+            showAlert('Системная ошибка!');
+            document.querySelector(".loading").style.display = "none";
+        }
+    });
+}
+
+    </script>
+</body>
 </html>
+
